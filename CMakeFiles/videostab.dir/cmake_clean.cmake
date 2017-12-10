acbcb9d85facaf7f1663dcca688a08a16660d25f
sagarverma <sagar15056@iiitd.ac.in>
2017-12-10 19:06:18 +0530

    first commit

---
+++ CMakeFiles/videostab.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/videostab.dir/videostab.cpp.o"
+  "videostab.pdb"
+  "videostab"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/videostab.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
